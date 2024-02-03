ba5910ae26bc56cdf0dd66d4764b7e0bf4c10847
ThaminduNR <thaminduniwantha777@gmail.com>
2024-02-03 10:12:53 +0530

    add btns

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,30 +4,40 @@ function Home() {
   return (
     <div className="flex justify-center">
       <form className="mt-[100px]">
-        <label>
-          User ID:
-          <input
-            type="text"
-            className="border-slate-400 border p-1 rounded-md  mx-3"
-          />
-        </label>
-        <label>
-          User Name:
-          <input
-            type="text"
-            className="border-slate-400 border p-1 rounded-md mx-3"
-          />
-        </label>
-        <label>
-          User Email:
-          <input
-            type="text"
-            className="border-slate-400 border p-1 rounded-md mx-5"
-          />
-        </label>
-        <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-800">
-          Save
-        </button>
+        <div>
+          <label>
+            User ID:
+            <input
+              type="text"
+              className="border-slate-400 border p-1 rounded-md  mx-3"
+            />
+          </label>
+          <label>
+            User Name:
+            <input
+              type="text"
+              className="border-slate-400 border p-1 rounded-md mx-3"
+            />
+          </label>
+          <label>
+            User Email:
+            <input
+              type="text"
+              className="border-slate-400 border p-1 rounded-md mx-5"
+            />
+          </label>
+        </div>
+        <div className="mt-[50px]">
+          <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-800 mx-4">
+            Save
+          </button>
+          <button className="px-4 py-2 font-bold text-white bg-gray-500 rounded hover:bg-blue-800 mx-4">
+            Update
+          </button>
+          <button className="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-blue-800">
+            Delete
+          </button>
+        </div>
       </form>
     </div>
   );
