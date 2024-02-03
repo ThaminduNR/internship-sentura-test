496201f8ff255e205fbab6e77c8212b4d7979345
ThaminduNR <thaminduniwantha777@gmail.com>
2024-02-03 10:16:25 +0530

    table add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center flex-col items-center">
       <form className="mt-[100px]">
         <div>
           <label>
@@ -39,6 +39,34 @@ function Home() {
           </button>
         </div>
       </form>
+      <div>
+        <table class="table-auto">
+          <thead>
+            <tr>
+              <th>User ID</th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+              <td>Malcolm Lockyer</td>
+              <td>1961</td>
+            </tr>
+            <tr>
+              <td>Witchy Woman</td>
+              <td>The Eagles</td>
+              <td>1972</td>
+            </tr>
+            <tr>
+              <td>Shining Star</td>
+              <td>Earth, Wind, and Fire</td>
+              <td>1975</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
